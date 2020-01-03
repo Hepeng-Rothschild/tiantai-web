cc75088037878c809abbd58e31b8db83f373fb01
WangChengAn <475293638@qq.com>
2020-01-03 13:27:05 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -6,42 +6,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    allPartner: getItem('allPartner') || null,
-    saleMan: getItem('saleMan') || null,
     user:getItem('user') || null
   },
   mutations: {
-    changePartnerData (state,data) {
-      state.allPartner = data
-    },
-    changeSaleMan(state,data) {
-      state.saleMan = data
-    },
     changeUser(state,data) {
       state.user = data
     }
   },
   actions: {
-    // 获取客户
-    async getPartner(context) {
-      const { data } = await Parse.Cloud.run("getPartner");
-      console.log('客户',data)
-      context.commit('changePartnerData',data[0])
-      setItem("allPartner", data[0]);
-    },
-    // getSaleMan 获取业务员
-    async getSaleMan(context) {
-      const {data}  = await Parse.Cloud.run("getSaleMan");
-      console.log('业务员',data)
-      context.commit('changeSaleMan',data[0])
-      setItem("saleMan", data[0]);
-    },
     // 获取 token
-    async getUser(context) {
+    async login(context) {
       const { data } = await Parse.Cloud.run("login");
-      const user = await Parse.User.become(data.sessionToken);
+      console.log('登录',data)
       context.commit('changeUser',data)
       setItem('user',data)
+      
+    },
+    async keepLogin(context) {
+      const user = await Parse.User.become(context.state.user.sessionToken);
       console.log("我带着token又登录啦", user);
     },
     async logout() {

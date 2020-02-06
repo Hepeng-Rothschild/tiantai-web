876fce716ce21843e0753919b42bdbcd71594fc1
李广松 <chenyue@chenyuedeMacBook-Pro.local>
2020-02-06 16:16:13 +0800

    路由拦截筛选

--- src/views/login.vue
+++ src/views/login.vue
@@ -9,7 +9,7 @@ export default {
         }
     },
     mounted(){
-        alert(this.$route.query.token + this.$route.query.path)
+        
         this.$Parse.User.become(this.$route.query.token).then(v =>{
             this.$router.push({path: this.$route.query.path})
         })
